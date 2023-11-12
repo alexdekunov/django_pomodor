5df66d227210654fa4bdbc4ed0326c9fa37824ac
alexdekunov <wresda@gmail.com>
2023-11-12 21:40:23 +0300

    serializer

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from services.models import Subscription
+
 
 # Create your views here.
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.object.all()
+
+    
