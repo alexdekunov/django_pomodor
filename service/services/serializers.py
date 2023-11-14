953f97314dadc6ed37e8736e99057b72e450000a
Aleksandr Dekunov <wresda@gmail.com>
2023-11-14 17:34:19 +0300

    Day2-141123

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,1 +1,15 @@
-class SubscriptionSer
+from rest_framework import serializers
+
+from services.models import Subscription
+
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    client_name = serializers.CharField(source='client.company_name')
+    email = serializers.CharField(source='client.user.email')
+
+
+    class Meta:
+        model = Subscription
+        fields = ('id', 'plan_id', 'client_name', 'email')
+
--- service/services/views.py
+++ service/services/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
+
 from services.models import Subscription
+from services.serializers import SubscriptionSerializer
+
 
 
 # Create your views here.
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.object.all()
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+
 
     
